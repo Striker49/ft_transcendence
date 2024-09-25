07b02c0341a4f96a91c59333b449666fd428a65a
Sebastien Roy <seroy@c2r2p9.42quebec.com>
2024-09-25 16:38:15 -0400

    new improved Pong version with classes

--- static/ball.js
+++ static/ball.js
@@ -1,15 +1,102 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import {ballCollision} from './collision.js'
+import {resetBallPosition} from './main.js'
 
-// Function to create the ball
-export function createBall() {
-    const geometry = new THREE.SphereGeometry(1.5, 15, 15);
+export class Ball extends THREE.Mesh{
+    constructor({radius,
+        width,
+        height,
+        color = '#00ff00',
+        velocity = {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        position = {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        zAcceleration
+    }) {
+        super(new THREE.SphereGeometry(radius, width, height),
+        new THREE.MeshStandardMaterial({ color })
+        )
 
-    // Create the ball
-    const material = new THREE.MeshBasicMaterial({ color: 0xffd700 });
-    const ball = new THREE.Mesh(geometry, material);
-	ball.position.x = 0;
-	ball.position.y = 0;
-	ball.position.z = -1.1;
+    this.radius = radius;
+    this.width = width;
+    this.height = height;
 
-    return ball ;
+    this.position.set(position.x, position.y, position.z);
+
+    this.bottom = this.position.y - this.radius / 2;
+    this.top = this.position.y + this.radius / 2;
+    this.right = this.position.x + this.radius / 2;
+    this.left = this.position.x - this.radius / 2;
+    this.front = this.position.z + this.radius / 2;
+    this.back = this.position.z - this.radius / 2;
+
+    this.velocity = velocity;
+    this.gravity = -0.002;
+    this.zAcceleration = zAcceleration;
+    }
+
+    updateSides() {
+        this.bottom = this.position.y - this.radius / 2;
+        this.top = this.position.y + this.radius / 2;
+        this.right = this.position.x + this.radius / 2;
+        this.left = this.position.x - this.radius / 2;
+        this.front = this.position.z + this.radius / 2;
+        this.back = this.position.z - this.radius / 2;
+    }
+
+    update(box, ground) {
+
+        this.updateSides();
+
+        if (this.zAcceleration)
+            this.velocity.z += 0.0003;
+
+        this.velocity.y += this.gravity;
+        this.position.x += this.velocity.x;
+        this.position.z += this.velocity.z;
+
+        this.paddleBounce(box);
+        this.sideBounce(ground);
+		this.outOfBounds(ground);
+    }
+
+    paddleBounce(box) {
+        //This is where we hit the box
+        if (ballCollision({
+			ball: this,
+            box: box
+		})){
+			// const friction = 0.5;
+			if (this.front > box.back && this.back < box.front)
+            	this.velocity.x *= -1;
+            // this.velocity.y = -this.velocity.y;
+
+        }
+		// console.log(this.velocity.x);
+    }
+
+	sideBounce(ground) {
+		//if the ball is inside the ground
+		if (this.right >= ground.left && this.left <= ground.right)
+		{
+			//if the ball touches the top or bottom side
+			if (this.velocity.z > 0 && (this.front + (this.velocity.z * 7)) >= ground.front)
+				this.velocity.z *= -1;
+			else if (this.velocity.z < 0 && this.back + (this.velocity.z * 7) <= ground.back)
+				this.velocity.z *= -1;
+		}
+	}
+
+	outOfBounds(ground) {
+		if (this.position.x < (ground.left - ground.width / 2) || this.position.z > (ground.front + ground.depth / 2))
+			resetBallPosition(this, 1);
+		else if (this.position.x > (ground.right + ground.width / 2) || this.position.z < (ground.back - ground.depth / 2))
+			resetBallPosition(this, 2);
+	}
 }
--- static/main.js
+++ static/main.js
@@ -1,212 +1,60 @@
-import * as THREE from 'three';
-import { createPaddles } from './paddles.js'; 
-import { createBall } from './ball.js';
-import { createField } from './field.js';
-import { createSides } from './sides.js';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { Box } from './box.js';
+import { Ball } from './ball.js';
 import { createText } from './text.js';
-import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
-const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 11, 0);
+scene.position.z = 1;
+
+const light = new THREE.DirectionalLight(0xffffff, 0.3);  // Adjust the intensity if needed
+const light2 = new THREE.AmbientLight(0xffffff, 1);  // Adjust the intensity if needed
+light.position.z = 1;
+light.position.y = 2;
+light.castShadow = true;
+scene.add(light, light2);
+
+const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true
+});
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 7500);
-renderer.setAnimationLoop(animate);
-camera.position.set(0, 0, 120 );
-// camera.lookAt(50, 50, 50);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const scene = new THREE.Scene();
+let speed = 0.1;
 
-// renderer.shadowMap.enabled = true;
-// renderer.shadowMap.type = THREE.PCSoftShadowMap;
+function inBounds({box1, box2}) {
+    box1.updateSides();
+    const collision = box1.front + speed <= box2.front && box1.back - speed >= box2.back;
+    return (collision);
+}
 
-const light = new THREE.AmbientLight(0xffffff);  // Ambient light
-const light2 = new THREE.SpotLight(0xffffff, 0.25);  // Ambient light
-light.position.set(0, 0, 20);
-light2.position.set(0, 0, 20);
-light2.castShadow = true;
-scene.add(light, light2);
+const cube = new Box({
+    width: 1,
+    height: 1,
+    depth: 3,
+    velocity: {
+        x: 0,
+        y: -0.01,
+        z: 0
+    },
+    position: {
+        x: -4.5,
+        y: -1.25,
+        z:0
+    }
+});
+cube.castShadow = true;
+scene.add(cube);
 
-// const lightHelper = new THREE.CameraHelper(light.shadow.camera);
-// scene.add(lightHelper);
-
-//Can orbit around the field with mouse click and zoom with scroll
-const controls = new TrackballControls( camera, renderer.domElement );
-// controls.noPan = true;
-// controls.target = new THREE.Vector3(0,0,0);
-
-// const loader = new GLTFLoader();
-
-// loader.load('resources/pong_table.glb', function (gltf) {
-//     const model = gltf.scene;
-
-//     // Modify the position of the loaded model
-//     model.position.set(0, 0, -1);  // Set the model's position to (x: 10, y: 0, z: 0)
-//     model.rotation.set(1.60, 0, 0);  // Set the model's position to (x: 10, y: 0, z: 0)
-    
-//     // Optional: scale or rotate the model
-//     model.scale.set(3, 3, 3);  // Scale the model if necessary
-//     scene.add(gltf.scene);
-// }, undefined, function (error) {
-//     console.error();
-// });
-
-
+let text, currentText;
 let scoreP1 = 0;
 let scoreP2 = 0;
-let text, currentText;
-
-
-let paddles = [];
-let balls = [];
-let field = [];
-let BB = [];
-let helpers = [];
-
-// updateScore(text);
-initObjects();
-init_BB();
-init_helper();
-
-function initObjects() {
-    const plane = createField();
-    field[0] = plane;
-    plane.receiveShadow = true;
-    scene.add(plane);
-
-    //Create sides
-    const {side1, side2} = createSides();
-    // field.push(side1, side2);
-    field[1] = side1;
-    field[2] = side2;
-    field[1].castShadow = true;
-    field[2].castShadow = true;
-
-    // Create and add the paddles to the scene
-    const {paddle1, paddle2} = createPaddles();
-    paddles[0] = paddle1;
-    paddles[1] = paddle2;
-    paddles[0].castShadow = true;
-    paddles[1].castShadow = true;
-
-
-    // Create and add the ball to the scene
-    const ball = createBall();
-    balls[0] = ball;
-    balls[0].castShadow = true;
-}
-
-//Create bouncing box
-function init_BB() {
-    const paddle1BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3());
-    paddle1BB.setFromObject(paddles[0]);
-    
-    const paddle2BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3());
-    paddle2BB.setFromObject(paddles[1]);
-    
-    const ballBB = new THREE.Box3(balls[0].computeBoundingSphere);
-    ballBB.setFromObject(balls[0]);
-    
-    const side1BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3());
-    side1BB.setFromObject(field[1]);
-
-    
-    const side2BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3());
-    side2BB.setFromObject(field[2]);
-    
-    const planeBB = new THREE.Box3(new THREE.Vector3(0), new THREE.Vector3(0), new THREE.Vector3(0));
-    planeBB.setFromObject(field[0]);
-    planeBB.min.multiplyScalar(1.25);
-    planeBB.max.multiplyScalar(1.25);
-    planeBB.translate(new THREE.Vector3(0, 0, 2));
-    BB.push(paddle1BB, paddle2BB, ballBB, side1BB, side2BB, planeBB);
-    
-}
-
-//Creating Hitboxes
-function init_helper() {
-    
-    const helperPaddle1 = new THREE.Box3Helper( BB[0], 0xffff00);
-    const helperPaddle2 = new THREE.Box3Helper( BB[1], 0xffff00);
-    const helperBall = new THREE.Box3Helper( BB[2], 0xffff00);
-    const helperSide1 = new THREE.Box3Helper( BB[3], 0xffff00);
-    const helperSide2 = new THREE.Box3Helper( BB[4], 0xffff00);
-    const helperField = new THREE.Box3Helper( BB[5], 0xffff00);
-    helpers.push(helperPaddle1, helperPaddle2, helperBall, helperSide1, helperSide2, helperField);
-}
-
-field.forEach(obj => {scene.add(obj)});
-paddles.forEach(obj => {scene.add(obj)});
-balls.forEach(obj => {scene.add(obj)});
-helpers.forEach(obj => {scene.add(obj)});
-// fieldBB.min.multiplyScalar(0.75);
-// fieldBB.translate(new THREE.Vector3(-4, -3, 0));
-
-
-
-
-// fieldBB.expandByScalar(-0.5);
-
-let paddleYDirection = 1;   // Direction for paddle 1
-let paddle2YDirection = -1;  // Direction for paddle 2
-
-let ballYDirection = 2;
-let ballXDirection = 2;
-
-const paddleSpeed = 0.005;   // Speed of the movement
-const ballSpeed = 0.001;   // Speed of the movement
-
-// Function to check collision
-function checkCollision() {
-    // Update bounding boxes
-    BB[0].setFromObject(paddles[0]);
-    BB[1].setFromObject(paddles[1]);
-    BB[2].setFromObject(balls[0]);
-
-    //paddle1BB
-    if (BB[0].intersectsBox(BB[2])) {
-        // ball.material.transparent = true;
-        // ball.material.opacity = 0.5;
-        balls[0].material.color.set(new THREE.Color(Math.random() * 0xffffff));
-        ballYDirection *= 1;
-        ballXDirection *= -1;
-    }
-    //paddle2BB
-    if (BB[1].intersectsBox(BB[2])) {
-        // ball.material.transparent = true;
-        // ball.material.opacity = 0.5;
-        balls[0].material.color.set(new THREE.Color(Math.random() * 0xffffff));
-        ballYDirection *= 1;
-        ballXDirection *= -1;
-        while (BB[4].intersectsBox(BB[2]))
-        {
-            balls[0].position.x += ballSpeed * ballXDirection;
-            balls[0].position.y += ballSpeed * ballYDirection;
-        }
-    }
-    if (BB[3].intersectsBox(BB[2])) {
-        // balls[0].material.color.set(new THREE.Color(Math.random() * 0xffffff));
-        ballYDirection *= -1;
-    }
-    if (BB[4].intersectsBox(BB[2])) {
-        // balls[0].material.color.set(new THREE.Color(Math.random() * 0xffffff));
-        ballYDirection *= -1;
-    }
-    if (!BB[5].intersectsBox(BB[2]))
-    {
-        if (ballXDirection > 0)
-            scoreP1++;
-        else
-            scoreP2++;
-        // updateScore(text);
-        balls[0].position.x = 0;
-        balls[0].position.y = 0;
-        balls[0].position.z = 0;
-    }
-}
 
 function updateScore(text) {
     createText(function (text) {
@@ -220,6 +68,7 @@ function updateScore(text) {
         // Add the new text
         text.castShadow = true;
         text.receiveShadow = true;
+        text.position.x = -2.75;
         scene.add(text);   // Add the new text to the scene
     
         // Store the reference to the new text
@@ -229,95 +78,148 @@ function updateScore(text) {
 
 }
 
-// movement - please calibrate these values
-let paddleXSpeed = 2;
-let paddleYSpeed = 0.005;
-let paddle1MovingUp = false;
-let paddle1MovingDown= false;
-let paddle2MovingUp = false;
-let paddle2MovingDown= false;
+updateScore();
 
-function animate() {
-    checkCollision();
-    requestAnimationFrame(animate);
-    
-    // Move the first paddle up and down
-    if (paddle1MovingUp)
-        paddles[0].position.y += paddleYSpeed;
-    if (paddle1MovingDown)
-        paddles[0].position.y -= paddleYSpeed;
-    
-    if (paddle2MovingUp)
-        paddles[1].position.y += paddleYSpeed;
-    if (paddle2MovingDown)
-        paddles[1].position.y -= paddleYSpeed;
 
-    // Move the second paddle up and down (with different speed)
-    // paddles[1].position.y += paddleSpeed * paddle2YDirection;
+const cube2 = new Box({
+    width: 1,
+    height: 1,
+    depth: 3,
+    velocity: {
+        x: 0,
+        y: -0.01,
+        z: 0
+    },
+    position: {
+        x: 4.5,
+        y: -1.25,
+        z: 0
+    },
+    color: 'red'
+});
+cube2.castShadow = true;
+scene.add(cube2);
 
-    balls[0].position.x += ballSpeed * ballXDirection;
-    balls[0].position.y += ballSpeed * ballYDirection;
+const ball = new Ball({
+    radius: 0.25,
+    width: 15,
+    height: 15,
+    velocity: {
+        x: 0.05,
+        y: -0.01,
+        z: 0.05
+    },
+    position: {
+        x: 0,
+        y: -1.5,
+        z: 0
+    },
+    color: 'yellow'
+});
+ball.castShadow = true;
+scene.add(ball);
 
-    // Reverse direction if the second paddle reaches a certain height
-    // if (paddles[1].position.y > 14 || paddles[1].position.y < -14) {
-    //     paddle2YDirection *= -1;
-    // }
+const ground = new Box({
+    width: 10, 
+    height: 0.5,
+    depth: 10,
+    color: '#0369a1',
+    position: {
+        x: 0,
+        y: -2,
+        z: 0
+    }
+    });
+ground.receiveShadow = true;
+scene.add(ground);
 
-    paddles[0].position.y = Math.max(-14, Math.min(14, paddles[0].position.y));
-    paddles[1].position.y = Math.max(-14, Math.min(14, paddles[1].position.y));
-
-    controls.update();
-
-    // Render the scene
-    renderer.render(scene, camera);
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+    right: {
+        pressed: false
+    },
+    up: {
+        pressed: false
+    },
+    down: {
+        pressed: false
+    }
 }
 
+window.addEventListener('keydown', (event) => {
+    switch(event.code) {
+        case 'KeyW':
+            keys.w.pressed = true;
+            break;        
+        case 'KeyA':
+            keys.a.pressed = true;
+            break;
+        case 'KeyS':
+            keys.s.pressed = true;
+            break;
+        case 'KeyD':
+            keys.d.pressed = true;
+            break;
+        case 'ArrowUp':
+            keys.up.pressed = true;
+            break;        
+        case 'ArrowLeft':
+            keys.left.pressed = true;
+            break;
+        case 'ArrowDown':
+            keys.down.pressed = true;
+            break;
+        case 'ArrowRight':
+            keys.right.pressed = true;
+            break;
+        case 'Space':
+            controls.reset();
+            break;
+    }
+})
 
-window.addEventListener("keydown", (event) => {
-    event.preventDefault();
-    if (event.code === "KeyW") {
-        paddle1MovingUp = true;
-	} 
-    if (event.code === "KeyS") {
-        paddle1MovingDown = true;
+window.addEventListener('keyup', (event) => {
+    switch(event.code) {
+        case 'KeyW':
+            keys.w.pressed = false;
+            break;        
+        case 'KeyA':
+            keys.a.pressed = false;
+            break;
+        case 'KeyS':
+            keys.s.pressed = false;
+            break;
+        case 'KeyD':
+            keys.d.pressed = false;
+            break;
+        case 'ArrowUp':
+            keys.up.pressed = false;
+            break;        
+        case 'ArrowLeft':
+            keys.left.pressed = false;
+            break;
+        case 'ArrowDown':
+            keys.down.pressed = false;
+            break;
+        case 'ArrowRight':
+            keys.right.pressed = false;
+            break;
     }
-    if (event.code === "ArrowUp") {
-        paddle2MovingUp = true;
-	} 
-    if (event.code === "ArrowDown") {
-        paddle2MovingDown = true;
-    }
-    // } else if (keyCode == 65 && paddles[0].position.x + paddleYSpeed > -25) {
-    //     paddles[0].position.x -= paddleXSpeed;
-    // } else if (keyCode == 68 && paddles[0].position.x + paddleYSpeed < 0) {
-    //     paddles[0].position.x += paddleXSpeed;
-    if (event.code === "KeyC") {
-        if (helpers[0].visible == true)
-            helpers.forEach(obj => {obj.visible = false});
-        else
-            helpers.forEach(obj => {obj.visible = true});
-    }
-    if (event.code === "Space") {
-        controls.reset();
-    }
-});
-
-window.addEventListener("keyup", (event) => {
-    event.preventDefault();
-    if (event.code === "KeyW") {
-        paddle1MovingUp = false;
-	}
-    if (event.code === "KeyS") {
-        paddle1MovingDown = false;
-    }
-    if (event.code === "ArrowUp") {
-        paddle2MovingUp = false;
-	}
-    if (event.code === "ArrowDown") {
-        paddle2MovingDown = false;
-    }
-});
-
+})
 
 window.addEventListener( 'resize', onWindowResize, false );
 
@@ -331,5 +233,47 @@ function onWindowResize(){
 }
 
 
-// Start the animation
+const enemies = [];
+
+let frames = 0;
+let spawnRate = 200;
+function animate() {
+    const animationID = requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+    //movement code
+    cube.velocity.z = 0;
+    if (keys.w.pressed && (cube.back - speed >= ground.back))
+        cube.velocity.z = -speed;
+    else if (keys.s.pressed && (cube.front + speed <= ground.front)) {
+        cube.velocity.z = speed;
+    }
+
+    cube2.velocity.z = 0;
+    if (keys.up.pressed && (cube2.back - speed >= ground.back))
+        cube2.velocity.z = -speed;
+    else if (keys.down.pressed && (cube2.front + speed <= ground.front)) {
+        cube2.velocity.z = speed;
+    }
+    
+    //updates cubes and ball
+    cube.update(ground);
+    cube2.update(ground);
+    if (ball.velocity.x < 0)
+        ball.update(cube, ground);
+    else
+        ball.update(cube2, ground);
+    frames++;
+}
+
+export function resetBallPosition(ball, winner) {
+    if (winner === 1)
+        scoreP2++;
+    else
+        scoreP1++;
+	ball.position.x = 0;
+	ball.position.y = -1.5;
+	ball.position.z = 0;
+    updateScore();
+}
 animate();
