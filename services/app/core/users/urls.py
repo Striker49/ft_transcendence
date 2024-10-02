261f67a5d4f5e250922cab7d5d04d10c7ec69f2f
Ziggy <ziggyvandeven@gmail.com>
2024-10-02 15:37:57 -0400

    Changed from Profile API to users api for creation of account.

--- services/app/core/users/urls.py
+++ services/app/core/users/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path, include
 from users import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('', views.UserViewSet)
 
 app_name = "users"
 urlpatterns = [
-	path("", views.UserViewSet.as_view({'get': 'list'}), name="users"),
-	path('login/', views.UserLoginApiView.as_view()),
-	path('<int:pk>/', views.UserDetail.as_view(), name='user-detail')
+    path('login/', views.UserLoginApiView.as_view()),  # Explicit login route
+    path("", include(router.urls)),  # UserViewSet will use /api/users/
+    path('<int:pk>/', views.UserDetail.as_view(), name='user-detail')  # User details route
 ]
+
+# from django.urls import path, include
+# from profiles import views
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register('profile', views.UserProfileViewSet)
+
+# urlpatterns = [
+# 	path('login/', views.UserLoginApiView.as_view()),
+# 	path('', include(router.urls), name='hello-viewset')
+# ]
