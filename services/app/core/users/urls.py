57077363f49c0c2cbd3257ac68f36ffbba220e94
Ziggy <ziggyvandeven@gmail.com>
2024-10-02 17:27:34 -0400

    updated urls.py

--- services/app/core/users/urls.py
+++ services/app/core/users/urls.py
@@ -11,15 +11,3 @@ urlpatterns = [
     path("", include(router.urls)),  # UserViewSet will use /api/users/
     path('<int:pk>/', views.UserDetail.as_view(), name='user-detail')  # User details route
 ]
-
-# from django.urls import path, include
-# from profiles import views
-# from rest_framework.routers import DefaultRouter
-
-# router = DefaultRouter()
-# router.register('profile', views.UserProfileViewSet)
-
-# urlpatterns = [
-# 	path('login/', views.UserLoginApiView.as_view()),
-# 	path('', include(router.urls), name='hello-viewset')
-# ]
