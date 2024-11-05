f2695eb015bee81ce49c6235940d926dd4b0013a
Sebastien Roy <seroy@c2r2p9.42quebec.com>
2024-11-05 09:19:32 -0500

    redirects to /gameConfig if refresh on /game
    added condition to change window.location.href to /gameConfig when refreshing the page on /game in DOMContentLoaded triggered event

--- services/frontend/core/src/game/main.js
+++ services/frontend/core/src/game/main.js
@@ -491,13 +491,3 @@ document.addEventListener('click', (event) => {
         scene.remove(winnerText);
     }
 })
-
-window.addEventListener("load", (event) => {
-    console.log("window.location", window.location.pathname);
-    if (window.location.pathname === "/game") {
-        console.log("entered if con");
-
-        // Redirect to another page if /game was reloaded
-        window.location.href = "/gameConfig";
-    }
-});
