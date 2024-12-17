8c5d0736b9cd8240a4f4e4ef7fad423955e32258
Sebastien Roy <seroy@c2r2p9.42quebec.com>
2024-12-17 17:01:54 -0500

    added oroy changes for gameconfig

--- services/frontend/core/src/pages/GameConfig.js
+++ services/frontend/core/src/pages/GameConfig.js
@@ -45,31 +45,34 @@ async function getUserProfile() {
 	}
 }
 
+
 async function getNbPlayer(queryName) {
-	let nbPlayer = false;
+    let nbPlayer = false;
     const params = new URLSearchParams(window.location.search);
-    if (params.get(queryName) == "1" || (!params.get(queryName) && localStorage.getItem("nbPlayer") == "1"))
-	{
-		console.debug("One player detected");
-		nbPlayer = true;
-		p2NameLabel = "";
-		p2NameField = "";
-		localStorage.setItem("nbPlayer", "1");
-	}
-	else if (params.get(queryName) == "2")
-	{
-		console.debug("Two players detected");
-		nbPlayer = false;
-		p2NameField = "<label class=\"d-flex justify-content-center my-2 fw-bold p-1\" id=\"p2Form\" for=\"player2\" class=\"form-label\"><span data-i18n-key=\"player\">Player</span> 2</label><input type=\"text\" class=\"form-control w-auto mx-auto\" name=\"player2\" id=\"player2\">";
-		localStorage.setItem("nbPlayer", "2");
-	}
-	else 
-	{
-		console.log("No player detected");
-		p2NameField = "<label class=\"d-flex justify-content-center my-2 fw-bold p-1\" id=\"p2Form\" for=\"player2\" class=\"form-label\"><span data-i18n-key=\"player\">Player</span> 2</label><input type=\"text\" class=\"form-control w-auto mx-auto\" name=\"player2\" id=\"player2\">";
-		// (!params.get(queryName) && localStorage.getItem("nbPlayer"))
-		return (localStorage.getItem("nbPlayer") == "1" ? true : false)
-	}
+    if (params.get(queryName) == "1")
+    {
+        console.log("One player detected");
+        nbPlayer = true;
+        p2NameLabel = "";
+        p2NameField = "";
+        localStorage.setItem("nbPlayer", "1");
+    }
+    else if (params.get(queryName) == "2")
+    {
+        console.log("Two players detected");
+        nbPlayer = false;
+        p2NameLabel = "<p><label id=\"p2Form\" for=\"player2\" class=\"form-label\"><span data-i18n-key=\"player\">Player</span> 2</label></p>";
+        p2NameField = "<p><input type=\"text\" class=\"form-control\" name=\"player2\" id=\"player2\"></p>";
+        localStorage.setItem("nbPlayer", "2");
+    }
+    else
+    {
+        console.log("No player detected");
+        p2NameLabel = "<p><label id=\"p2Form\" for=\"player2\" class=\"form-label\"><span data-i18n-key=\"player\">Player</span> 2</label></p>";
+        p2NameField = "<p><input type=\"text\" class=\"form-control\" name=\"player2\" id=\"player2\"></p>";
+        // (!params.get(queryName) && localStorage.getItem("nbPlayer"))
+        return (localStorage.getItem("nbPlayer") == "1" ? true : false)
+    }
     return (nbPlayer);
 }
 
@@ -88,43 +91,44 @@ export default class extends Abstract {
 		username = (userData ? userData.username : localTranslations["playerOne"])
 		username2 = localTranslations["playerTwo"] || "Player 2";
 		return `
-			<div class="container bg-dark bg-opacity-75 rounded-5 p-5">
-				<div class="bg-info bg-opacity-50 border border-5 border-info rounded-5 text-black fw-bold">
-					<div class="row m-0">
-						<a href="/select" class="bg-info rounded-4 px-3 py-2 corner-back text-decoration-none text-black" style="width: 50px;" data-link>\<\<</a>
+		<div class="container bg-dark bg-opacity-75 rounded-5 p-5">
+			<div class="bg-info bg-opacity-50 border border-5 border-info rounded-5 text-black fw-bold">
+				<div class="row m-0">
+					<a href="/select" class="bg-info rounded-4 px-3 py-2 corner-back text-decoration-none text-black" style="width: 50px;" data-link>\<\<</a>
+				</div>
+				<div id="game-config" class="row m-0 mt-4 p-2 bg-info">
+					<div class="col-5 col-md-4 text-end p-4 fst-italic">
+						<p data-i18n-key="playerOne">Player 1</p>
+						${p2NameLabel}
+						<p><label for="winRange" class="form-label"><span data-i18n-key="numberOfWins">Number of wins</span></label></p>
+						<p><span data-i18n-key="theme" role="text">Theme</span></p>
+						<p><label for="powerUps">Power-Ups</label></p>
 					</div>
-					<div id="game-config" class="row m-0 mt-4 p-2 bg-info">
-						<div class="col-5 col-md-4 text-end p-4 fst-italic">
-							<p data-i18n-key="playerOne">Player 1</p>
-							${p2NameLabel}
-							<p><label for="winRange" class="form-label"><span data-i18n-key="numberOfWins">Number of wins</span></label></p>
-							<p><span data-i18n-key="theme" role="text">Theme</span></p>
-							<p><label for="powerUps">Power-Ups</label></p>
-						</div>
-						<div class="col-7 col-md-8 bg-alt-blue rounded-4 p-4">
-							<p><span id="player1" role="text" data-skip-i18n="false" data-i18n-key="playerOne">${username}</span></p>
-							${p2NameField}
-							<p>
-								<input type="range" class="form-range w-75" min="1" max="11" value="${localStorage.getItem('numberOfWins') || '3'}" id="winRange">
-								<span id="demo" class="float-end">${localStorage.getItem('numberOfWins') || '3'}</span>
-							</p>
-							<p>
-								<select id="theme" class="form-select auto-width-select" aria-label="Winter">
-									<option data-i18n-key="none" value="None" ${localStorage.getItem('theme') === 'None' ? 'selected' : ''}>None</option>
-									<option data-i18n-key="custom" value="Custom" ${localStorage.getItem('theme') === 'Custom' ? 'selected' : ''}>Custom</option>
-									<option data-i18n-key="christmas" value="Christmas" ${localStorage.getItem('theme') === 'Christmas' ? 'selected' : ''}>Christmas</option>
-									<option data-i18n-key="halloween" value="Halloween" ${localStorage.getItem('theme') === 'Halloween' ? 'selected' : ''}>Halloween</option>
-									<option data-i18n-key="winter" value="Winter" ${localStorage.getItem('theme') === 'Winter' ? 'selected' : ''}>Winter</option>
-								</select>
-							</p>
-							<p><input type="checkbox" id="powerUps" value="false"></p>
-						</div>
-					<div class="row mx-0 my-4 justify-content-center">
-						<a href="#" data-i18n-key="start" id="startBtn" class="btn btn-dark rounded-pill px-4 bg-orange text-dark fw-bold box-shadow border-0 w-auto">Start</a>
+					<div class="col-7 col-md-8 bg-alt-blue rounded-4 p-4">
+						<p><span id="player1" role="text" data-skip-i18n="false" data-i18n-key="playerOne">${username}</span></p>
+						${p2NameField}
+						<p>
+							<input type="range" class="form-range w-75" min="1" max="11" value="${localStorage.getItem('numberOfWins') || '3'}" id="winRange">
+							<span id="demo" class="float-end">${localStorage.getItem('numberOfWins') || '3'}</span>
+						</p>
+						<p>
+							<select id="theme" class="form-select auto-width-select" aria-label="Winter">
+								<option data-i18n-key="none" value="None" ${localStorage.getItem('theme') === 'None' ? 'selected' : ''}>None</option>
+								<option data-i18n-key="custom" value="Custom" ${localStorage.getItem('theme') === 'Custom' ? 'selected' : ''}>Custom</option>
+								<option data-i18n-key="christmas" value="Christmas" ${localStorage.getItem('theme') === 'Christmas' ? 'selected' : ''}>Christmas</option>
+								<option data-i18n-key="halloween" value="Halloween" ${localStorage.getItem('theme') === 'Halloween' ? 'selected' : ''}>Halloween</option>
+								<option data-i18n-key="winter" value="Winter" ${localStorage.getItem('theme') === 'Winter' ? 'selected' : ''}>Winter</option>
+							</select>
+						</p>
+						<p><input type="checkbox" id="powerUps" value="false"></p>
 					</div>
 				</div>
+				<div class="row mx-0 my-4 justify-content-center">
+					<a href="#" data-i18n-key="start" id="startBtn" class="btn btn-dark rounded-pill px-4 bg-orange text-dark fw-bold box-shadow border-0 w-auto">Start</a>
+				</div>
 			</div>
-		`;
+		</div>
+	`;
 	}
 }
 
