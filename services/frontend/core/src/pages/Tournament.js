d3bad16702d366fdde855e161f4a6a12fa2146d6
oroy-dev <olivierroy123@gmail.com>
2025-01-02 19:15:05 -0500

    tournament diagram + some tests

--- services/frontend/core/src/pages/Tournament.js
+++ services/frontend/core/src/pages/Tournament.js
@@ -8,14 +8,32 @@ export default class extends Abstract {
 
 	async getHtml() {
 		return `
-			<div id="game-screen" class="container bg-secondary rounded-5 p-5" style="width: 960px; height: 540px;">
-				<div class="row justify-content-center align-items-center bg-dark rounded-5 p-5 h-100 mx-auto">
-					<div class="col-8">
-						<img src="/src/assets/tournament.png" alt="Tournament diagram" class="w-100">
+			<div class="container bg-dark bg-opacity-75 rounded-5 p-5">
+        		<div class="bg-info bg-opacity-50 border border-5 border-info rounded-5 text-black">
+					<div class="row mb-4 justify-content-center">
+						<h4 class="bg-info w-auto px-4 py-2 rounded-bottom-4 fw-bold" data-i18n-key="about">Tournament</h4>
 					</div>
-					<div class="col-4">
-						<a href="/game" role="button" data-i18n-key="start" class="btn btn-success d-block" data-link>Start Game</a>
-					</div>	
+					<div class="row m-0 mt-4 p-4 bg-info">
+						<div class="col tournament-column">
+							<div class="tournament-block">Player 1</div>
+							<div class="tournament-block"><input type="text"></div>
+							<div class="tournament-block"><input type="text"></div>
+							<div class="tournament-block"><input type="text"></div>
+						</div>
+						<div class="col tournament-column">
+							<div class="tournament-block"></div>
+							<div class="tournament-block"></div>
+						</div>
+						<div class="col tournament-column">
+							<div class="tournament-block"></div>
+						</div>
+					</div>
+					<div class="row mx-0 my-4 justify-content-center position-relative">
+						<div class="position-absolute">
+							<input type="checkbox" name="allCPU" id="allCPU">
+						</div>
+						<a href="#" data-i18n-key="start" id="startBtn" class="btn btn-dark rounded-pill px-4 bg-orange text-dark fw-bold box-shadow border-0 w-auto z-1">Start</a>
+					</div>
 				</div>
 			</div>
 		`;
